34f75b60507103a2e148c914154ae5ef9527462f
Kortom-Tom <55277950+Kortom-Tom@users.noreply.github.com>
2020-01-02 17:34:24 +0100

    Update Conversion.js

--- WebsiteCurrency/Conversion.js
+++ WebsiteCurrency/Conversion.js
@@ -1,4 +1,6 @@
 function calcution()
 {
-    
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "CLICK ME";                   // Insert text
+    document.body.appendChild(btn);               // Append <button> to <body> 
 }
